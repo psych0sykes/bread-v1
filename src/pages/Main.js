bf45f86de9ea330ed3f8d1e4924204d56c903a15
psych0sykes <blakesykes13@gmail.com>
2020-02-17 21:54:20 -0700

    Added some divs to click to trigger sound, installed tone.js for dev. Added enable audio function.

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,24 @@
 import React from "react";
+import * as Tone from "tone";
+
+//attach a click listener to a play button
+const enableAudio = async () => {
+	await Tone.start()
+	console.log('audio is ready')
+}
+
+//create a synth and connect it to the master output (your speakers)
+const synth = new Tone.Synth().toDestination();
+
+//play a middle 'C' for the duration of an 8th note
+const start = () => synth.triggerAttackRelease("C4", "8n");
 
 function Main (props) {
     return (
-        <div></div>
+        <div>
+        <div onClick={start} style={{backgroundColor: "black",width: "300px",height: "300px"}}></div>
+        <div onClick={enableAudio} style={{backgroundColor: "red",width: "50px",height: "50px"}}></div>
+        </div>
     )
 }
 
