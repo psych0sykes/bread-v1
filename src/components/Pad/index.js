c1bffef28337c951c13bd4057d2f18cf71168e64
psych0sykes <blakesykes13@gmail.com>
2020-03-15 23:30:50 -0600

    Installed styled-components library to use template literals for css animations and key frames.

--- src/components/Pad/index.js
+++ src/components/Pad/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import * as Tone from 'tone';
+import styled, { keyframes } from 'styled-components'
 import './style.css';
 
 // ||===== PAD COMPONENT =====||
@@ -15,23 +15,38 @@ function Pad(props) {
 
     const [padStyle, setPadStyle] = useState({})
 
+    const initialStyle = {backgroundColor: "orangered"}
+
     // function to assign initial state to each pad from the props.notes array    
     useEffect(() => {
-        const initialStyle = async () => {
-            let style = {
-                backgroundColor: "orangered"
-            }
+        const setInitialStyle = async () => {
             var initialObj = {}
             for(var i = 0; i < pads.length; i++){
-                initialObj[pads[i]] = style;
+                initialObj[pads[i]] = initialStyle;
             }
             console.log(initialObj);
             setPadStyle(initialObj)
         }
-        initialStyle();
+        setInitialStyle();
     }, [])
 
     // function to randomly create an animation
+    const animate = (pad) => {
+        // @keyframes [pad] {0% {} 100%{}}
+
+        let start = ``;
+        let end = ``;
+
+        var animation = keyframes`
+        0% {
+            ${start}
+        }
+        100% {
+            ${end}
+        }
+        `;
+
+    }
 
     // noteTrigger - hande the click
     const noteTrigger = (pad, cb) => {
@@ -39,7 +54,7 @@ function Pad(props) {
     }
 
     const createPads = pads.map((pad) => 
-    <div style={padStyle[pad]} key={pad} className="pad" onClick={() => {
+    <div style={padStyle[pad]} key={pad} className={"pad"} onClick={() => {
         noteTrigger(pad);
         props.padClick(pad);}}>{pad}</div>
     )
