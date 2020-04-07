05db604f3d5c482b6bc4b12403d962160b9240b9
psych0sykes <blakesykes13@gmail.com>
2020-04-07 01:51:07 -0600

    Added onAnimationEnd synthetic event to set padState back to blank when the animtion for a pad is complete.

--- src/components/PadGrid/index.js
+++ src/components/PadGrid/index.js
@@ -17,24 +17,24 @@ const convertArrayToObject = (array) => {
     return array.reduce((obj, item) => {
       return {
         ...obj,
-        [item]: "test",
+        [item]: "",
       };
     }, initialValue);
   };
 
 function PadGrid(props) {
     const pads = props.notes
-    const [padState, padDispatch] = useState(() => convertArrayToObject(props.notes))
+    const [padState, setPadState] = useState(() => convertArrayToObject(props.notes))
 
     // noteTrigger - hande the click
     const noteTrigger = (pad, cb) => {
-        padDispatch({...padState, [pad]: "clickedPad"})
+        setPadState({...padState, [pad]: "clickedPad"})
         console.log(padState)
         console.log("clicked " + pad);
     }
 
     const createPads = pads.map((pad_id) => 
-    <div key={pad_id} style={{backgroundColor: props.color}} className={"pad " + padState[pad_id]} onClick={() => {
+    <div key={pad_id} style={{backgroundColor: props.color}} onAnimationEnd={() => setPadState({...padState, [pad_id]: ""})} className={"pad " + padState[pad_id]} onClick={() => {
         noteTrigger(pad_id);
         props.padClick(pad_id);}}>{pad_id}</div>
     )
